5b82eb4af32c40dd82c6b7bba07acfd52b0fa363
swest1 <shaunwest@quickenloans.com>
2014-12-29 11:41:56 -0500

    moved header/footer control to the front end

--- assets/js/route-defaults.js
+++ assets/js/route-defaults.js
@@ -3,6 +3,8 @@
 
   angular.module('hansei.routes').
     constant('appStateDefaults', {
-      title: 'Hansei'
+      title: 'Hansei',
+      headerUrl: 'templates/header-footer/header.html',
+      footerUrl: 'templates/header-footer/footer.html'
     });
 })();
