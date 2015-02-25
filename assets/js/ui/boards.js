c6a8a61fecbf622535ae4a11edefa92bb17e806f
Adam Bellinson <adambellinson@quickenloans.com>
2015-02-25 09:55:36 -0500

    hook board:create more better

--- assets/js/ui/boards.js
+++ assets/js/ui/boards.js
@@ -3,10 +3,10 @@
 
   angular.module('hansei.ui')
 
-  .controller('BoardsCtrl', ['$scope', 'boards', 'user', 'api', 'config',
-    function($scope, boards, user, api, config) {
+  .controller('BoardsCtrl', ['$scope', 'boards', 'user', 'api', 'config', 'eventerFactory',
+    function($scope, boards, user, api, config, eventerFactory) {
 
-      if (!user.token()) return;
+      // if (!user.token()) return;
 
       $scope.boards  = boards;
       $scope.colsets = config.colsets();
@@ -14,9 +14,10 @@
       // reverse the array to order boards by descending create date
       $scope.boards.reverse();
 
-      api.hook('board:create', $scope, function(board) {
+      eventerFactory().event('board:create', function(board) {
         $scope.boards.push(board);
-      });
+      }).hook($scope);
+
     }])
 
 })();
