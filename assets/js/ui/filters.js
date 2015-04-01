4eaece0177dc12700d8b5da2f9b26ff0c18b93e8
Adam Bellinson <adambellinson@quickenloans.com>
2015-04-01 17:11:18 -0400

    dates in board list!

--- assets/js/ui/filters.js
+++ assets/js/ui/filters.js
@@ -1,26 +1,38 @@
 (function() {
   'use strict';
 
+  var monthmap = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+    'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+
   angular.module('hansei.ui')
 
-  .filter('formatTime', function() {
+  .filter('formatDate', function() {
+    return function(date) {
+      date = new Date(date);
+      var ret = monthmap[date.getMonth()] + ' ' + date.getDate();
+      if (date.getFullYear() !== (new Date()).getFullYear()) {
+        ret += ', ' + date.getFullYear();
+      }
+      return ret;
+    };
+  })
 
+  .filter('formatTime', function() {
     return function(totalSeconds) {
       var mins = parseInt(totalSeconds / 60);
       var secs = totalSeconds % 60;
       if (secs < 10) secs = '0' + secs;
       return mins + ':' + secs;
     };
-
   })
 
   .filter('obfuscated', function() {
-
     return function(txt) {
       if (!txt) return null;
       return txt.replace(/./g, '*');
     };
-
   });
 
   // .filter('myVotes', ['user', function(user) {
